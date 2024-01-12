eedc4ddbfadd629caad1a43628e9bb412d2506bc
Aditya . <adityabagari77@gmail.com>
2024-01-12 18:48:12 +0530

    12-01-2024 06:48 PM

---
+++ oop-CLC.py
@@ -0,0 +1,53 @@
+class Calculator:
+        def repeater():
+          a = input("Enter the First Number : ")
+          if a == "":
+              print("\n=========================! BLANK INPUT !=========================\n=========================! RESTART !=========================\n")
+              Calculator.repeater()
+          else:
+              b = input("Enter the Second Number : ")
+              if b == "":
+                  print("\n=========================! BLANK INPUT !=========================\n=========================! RESTART !=========================\n")
+                  Calculator.repeater()
+          a,b = int(a),int(b)                 
+          operator = int(
+              input('''
+          Enter the operator : 
+            1. Addition
+            2. Subtraction
+            3. Multiplication
+            4. Division
+            Please entetr the Index Number.
+
+          Operator : '''))
+
+          print("")
+
+          if a == "" or b == "":
+              print("!===============! Invalid Input Restart !===============")
+          if operator == 1:
+            print(f"Sum : {a+b}")
+            Calculator.repeater()
+
+          elif operator == 2:
+            print(f"Subtracted Values : {a-b} \n")
+            Calculator.repeater()
+
+          elif operator == 3:
+            print(f"Multiplied Values : {a*b} \n")
+            Calculator.repeater()
+
+          elif operator == 4:
+            if b==0:
+                print("You can't divide by zero. \n")
+                Calculator.repeater()
+            else:
+              print(f"Divided Values : {a/b} \n")
+              Calculator.repeater()
+
+          else:
+            print("Invalid Operators choose between 1 to 4 \n")
+            Calculator.repeater()
+
+while True:
+    Calculator.repeater()
